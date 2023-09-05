a4ab675279dbf91c280c641911f471a56526b880
lulkien <kien.luuhoang.arch@gmail.com>
2023-09-06 00:46:09 +0700

    Modify

--- scripts/get-active-window.sh
+++ scripts/get-active-window.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
-  stdbuf -o0 awk -F '>>|,' -e '/^activewindow>>/ {print $3}'
+hyprctl activewindow -j | jq --raw-output .title
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 awk -F '>>|,' '/^activewindow>>/{print $3}'
